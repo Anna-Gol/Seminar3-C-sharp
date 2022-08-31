ea8f931a230153375b91322b0013179ccfd03020
Anna Golikova <annagolikovast@gmail.com>
2022-08-31 16:51:40 +0400

    23 done

---
+++ Задача 23/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Task2
+{
+    public static void Main(string[] args)
+    {
+        thirdPower();
+    }
+
+    static void thirdPower()
+    {
+        Console.Write("Enter number : ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Таблица кубов");
+
+        for (int i = 1;i<=number;i++)
+        {
+            Console.WriteLine(Math.Pow(i,3));
+        }
+
+    }
+}
