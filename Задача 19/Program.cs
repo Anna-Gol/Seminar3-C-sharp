3f4a712f92eb0fe8d93c20df056fdb187208a79e
Anna Golikova <annagolikovast@gmail.com>
2022-08-31 19:40:29 +0400

    19

---
+++ Задача 19/Program.cs
@@ -0,0 +1,43 @@
+﻿public class Task3
+{
+    public static void Main(string[] args)
+    {
+        palindromeCheck();
+    }
+
+    static void palindromeCheck()
+    {
+        Console.Write("Enter number : ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        int temp = number;
+        int rev = 0;
+        int div = 0;
+
+        if (number < 0)
+        {
+             Console.WriteLine("Число не удовлетворяет заданным критериям");
+        
+        }
+
+        else
+        while (number > 0)
+        {
+            div = number % 10;
+            rev = rev * 10 + div;
+            number = number / 10;
+
+            if (temp == rev) 
+            {
+                Console.WriteLine("Число является палиндромом");
+            }
+
+            else
+            {
+                Console.WriteLine("Число не является палиндромом");
+            }
+        }
+
+
+    }
+
+}
